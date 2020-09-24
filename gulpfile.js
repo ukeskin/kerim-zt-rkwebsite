45eac429d573522403cf27dfd5835b6969673421
Umut Keskin <ukeskin2626@gmail.com>
2020-09-24 16:23:17 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ function watch() {
     });
     gulp.watch('src/scss/**/*.scss', style)
     gulp.watch('src/*.html').on('change', browserSync.reload);
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,43 @@
-$(document).ready(function() {
-    $("#play").click(function() {
-       $(this).addClass("hide");
-       $("#pause").removeClass("hide");
-    });
-    $("#pause").click(function() {
-       $(this).addClass("hide");
-       $("#play").removeClass("hide");
-    });
+$(document).ready(function () {
+	$("#play").click(function () {
+		$(this).addClass("hide");
+		$("#pause").removeClass("hide");
+		$("#player").trigger("load");
+		$("#player").trigger("play");
+	});
+	$("#pause").click(function () {
+		$(this).addClass("hide");
+		$("#play").removeClass("hide");
+		$("#player").trigger("pause");
+	});
+});
 
-    $("#play2").click(function() {
-      $(this).addClass("hide");
-      $("#pause2").removeClass("hide");
-   });
-   $("#pause2").click(function() {
-      $(this).addClass("hide");
-      $("#play2").removeClass("hide");
-   });
+$(document).ready(function () {
 
+	$("#play2").click(function () {
+		$(this).addClass("hide");
+		$("#pause2").removeClass("hide");
+		$("#player2").trigger("load");
+		$("#player2").trigger("play");
+	});
+	$("#pause2").click(function () {
+		$(this).addClass("hide");
+		$("#play2").removeClass("hide");
+		$("#player2").trigger("pause");
+	});
+});
 
-   $("#play3").click(function() {
-      $(this).addClass("hide");
-      $("#pause3").removeClass("hide");
-   });
-   $("#pause3").click(function() {
-      $(this).addClass("hide");
-      $("#play3").removeClass("hide");
-   });
-  });
-  
-  console.log("2131231")
+$(document).ready(function () {
+
+	$("#play3").click(function () {
+		$(this).addClass("hide");
+		$("#pause3").removeClass("hide");
+		$("#player3").trigger("load");
+		$("#player3").trigger("play");
+	});
+	$("#pause3").click(function () {
+		$(this).addClass("hide");
+		$("#play3").removeClass("hide");
+		$("#player3").trigger("pause");
+	});
+});
